c8d3f30f85958f54f998f9bfeb5ddf01fc596ca1
Ellen Mey <eisforellen@gmail.com>
2016-09-01 10:53:30 -0400

    Add delete functionality

--- toDoSwift/toDoSwift/ViewController.swift
+++ toDoSwift/toDoSwift/ViewController.swift
@@ -19,14 +19,43 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
         collectionView?.alwaysBounceVertical = true
         
         collectionView?.registerClass(TaskCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.registerClass(TaskHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerId")
     }
     
+    var tasks = ["Buy Groceries", "Pay Bills"]
+    
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return tasks.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCellWithReuseIdentifier("cellId", forIndexPath: indexPath)
+        let taskCell = collectionView.dequeueReusableCellWithReuseIdentifier("cellId", forIndexPath: indexPath) as! TaskCell
+        taskCell.nameLabel.text = tasks[indexPath.item]
+        
+        let cSelector = #selector(ViewController.reset(_:))
+        let RightSwipe = UISwipeGestureRecognizer(target: self, action: cSelector)
+        RightSwipe.direction = UISwipeGestureRecognizerDirection.Right
+        taskCell.addGestureRecognizer(RightSwipe)
+        
+        return taskCell
+    }
+    
+    func reset(sender: UISwipeGestureRecognizer) {
+        let cell = sender.view as! UICollectionViewCell
+        let index = collectionView?.indexPathForCell(cell)!.item
+        tasks.removeAtIndex(index!)
+        collectionView?.reloadData()
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+        
+        if cell?.backgroundColor == UIColor.greenColor() {
+            cell?.backgroundColor = UIColor.clearColor()
+        } else {
+            cell?.backgroundColor = UIColor.greenColor()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,11 +66,27 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(view.frame.width, 50)
     }
+    
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "headerId", forIndexPath: indexPath) as! TaskHeader
+        header.viewController = self
+        return header
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSizeMake(view.frame.width, 100)
+        
+    }
+    
+    func addNewTask(taskName: String) {
+        tasks.append(taskName)
+        collectionView?.reloadData()
+    }
 
 
 }
 
-class TaskCell: UICollectionViewCell {
+class BaseCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
@@ -51,19 +96,65 @@ class TaskCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setupViews() {
+        
+    }
+}
+
+class TaskHeader: BaseCell {
+    
+    var viewController: ViewController?
+    
+    let taskNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Task Name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .RoundedRect
+        return textField
+    }()
+    
+    let addTaskButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.setTitle("Add Task", forState: .Normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func setupViews() {
+        
+        addSubview(taskNameTextField)
+        addSubview(addTaskButton)
+        
+        addTaskButton.addTarget(self, action: #selector(self.addTask), forControlEvents: .TouchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-15-[v0]-[v1(80)]-15-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": taskNameTextField, "v1": addTaskButton]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-24-[v0]-24-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": taskNameTextField]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": addTaskButton]))
+        
+    }
+    
+    func addTask() {
+        viewController?.addNewTask(taskNameTextField.text!)
+        taskNameTextField.text = ""
+    }
+}
+
+class TaskCell: BaseCell {
+    
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Sample Task"
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFontOfSize(14)
         return label
     }()
     
-    func setupViews() {
+    override func setupViews() {
         
         addSubview(nameLabel)
         
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
-        a ddConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-15-[v0]-15-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
         
     }
 }
